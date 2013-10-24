eba5f552cc200c1b1187bd8f9a3852d3c9237548
testvidya11 <vidyatest13@gmail.com>
2013-10-24 15:07:07 +0530

    Update runTests.cmd

--- runTests.cmd
+++ runTests.cmd
@@ -1,1 +1,1 @@
-.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe .\Shippable.CSharpSample.Tests\bin\Debug\unittest.dll /noshadow /result:shippable\testresults\testresults.xml
+.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe .\UnitTests.cs\bin\Debug\unittest.dll /noshadow /result:shippable\testresults\testresults.xml
